12573742343af4ea97e3e8e4126a24b3778b453a
faisal-foyez <faisalislam64@gmail.com>
2024-09-15 21:07:25 +0600

    Update Immer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import QueingState from './components/QueingState';
+import Immer from './components/ImmerUse';
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <QueingState />
+      <Immer />
     </>
   )
 }
