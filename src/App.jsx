e62043f0aa46a03239c8d7a3801198d27bcada39
faisal-foyez <faisalislam64@gmail.com>
2024-09-16 18:00:12 +0600

    Implement Preserving and resetting state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import Immer from './components/ImmerUse';
+import Counter from './components/Counter';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-
-
+  const [isPaused, setIsPaused] = useState(false);
   return (
-    <>
-      <Immer />
-    </>
-  )
+    <div>
+      {isPaused ? (
+        <p>See you later!</p> 
+      ) : (
+        <Counter /> 
+      )}
+      <label>
+        <input
+          type="checkbox"
+          checked={isPaused}
+          onChange={e => {
+            setIsPaused(e.target.checked)
+          }}
+        />
+        Take a break
+      </label>
+    </div>
+  );
 }
 
 export default App
