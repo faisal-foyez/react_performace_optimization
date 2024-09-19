b49a503b20e6ae1a652561fea5dc732b8f66d731
faisal-foyez <faisalislam64@gmail.com>
2024-09-19 16:37:31 +0600

    Add Video Player in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import RefImperative from './components/RefImperative';
+import VideoPlayer from './components/VideoPlayer';
 import './App.css'
-
+import { useState } from 'react';
 function App() {
-  return(
-    <RefImperative />
-  )
+ 
+  
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <>
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
+      <VideoPlayer
+        isPlaying={isPlaying}
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+      />
+    </>
+  );
+  
 }
 
 export default App
