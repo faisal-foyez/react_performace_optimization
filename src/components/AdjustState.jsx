3f8436976f44d339a74576f57eb3f5100f21cd6c
faisal-foyez <faisalislam64@gmail.com>
2024-09-20 16:56:20 +0600

    Add Adjust State

---
+++ src/components/AdjustState.jsx
@@ -0,0 +1,17 @@
+export default function AdjustState ({items}) {
+  const [isReverse, setIsReverse] = useState(false);
+  const [selection, setSelection] = useState(null);
+
+  // Better: Adjust the state while rendering
+  const [prevItems, setPrevItems] = useState(items);
+  if (items !== prevItems) {
+    setPrevItems(items);
+    setSelection(null);
+  }
+  
+  return(
+    <div>
+      {selection}
+    </div>
+  )
+}
