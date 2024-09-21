09df1daa2a87660bc17b91fdb0efe237452bff2e
faisal-foyez <faisalislam64@gmail.com>
2024-09-22 00:37:02 +0600

    Implement Chat room

---
+++ src/components/ChatRoom.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useEffectEvent } from "react";
+
+function ChatRoom({ roomId, theme }) {
+  const onConnected = useEffectEvent(() => {
+    showNotification('Connected!', theme);
+  });
+
+  useEffect(() => {
+    const connection = createConnection(serverUrl, roomId);
+    connection.on('connected', () => {
+      onConnected();
+    });
+    connection.connect();
+    return () => connection.disconnect();
+  }, [roomId]);
+
+  return <h1>Welcome to the {roomId} room!</h1>
+}
